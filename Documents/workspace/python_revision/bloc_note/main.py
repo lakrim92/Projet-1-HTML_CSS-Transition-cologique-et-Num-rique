8870e535356ae6d46cde99081ce2ad17a7585f99
Lakrim92 <gdietrich@live.fr>
2024-03-03 19:38:38 +0100

    depot main.py

---
+++ Documents/workspace/python_revision/bloc_note/main.py
@@ -0,0 +1,22 @@
+class Note:
+    def __init__(self):
+        self.notes = {}
+
+    def ajouter_note(self, clef, valeur):
+        self.notes[clef] = valeur
+
+    def consulter_note(self, clef):
+        return self.notes[clef]
+
+# Exemple d'utilisation de la classe Note
+if __name__ == "__main__":
+    bloc_note = Note()
+
+    bloc_note.ajouter_note("Tâche 1", "Faire les courses")
+    bloc_note.ajouter_note("Tâche 2", "Rédiger le rapport")
+
+    try:
+        clef = input("Entrez la clé de la note à consulter : ")
+        print("Note correspondante :", bloc_note.consulter_note(clef))
+    except KeyError:
+        print("La clé saisie n'existe pas.")
